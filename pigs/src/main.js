9f913ef53d86984309bf9a8239c536f1563823a4
William Estoque <william.estoque@gmail.com>
2012-05-05 18:37:49 -0700

    Made it work.

--- pigs/src/main.js
+++ pigs/src/main.js
@@ -32,27 +32,21 @@ function HappyLayer () {
   var s = Director.sharedDirector.winSize;
   var self = this;
 
+  // Add Bird
+  var birdCount = 3;
+  this.birds = []
+  for(var i=0; i<birdCount; i++) {
+    var bird = new Bird(s, this);
+    bird.position = new geo.Point(320, 320);
+    this.addChild(bird);
+    this.birds.push(bird);
+  }
+
   // Add Pig
   var pig = new Pig();
   pig.position = new geo.Point(320, 320);
   this.addChild(pig);
   this.pig = pig;
-
-  var bird = new Bird();
-  bird.position = new geo.Point(10, 10);
-  this.addChild(bird);
-  this.bird = bird;
-  //this.birds.push(bird);
-
-  // Add Bird
-  var birdCount = 5;
-  this.birds = []
-  for(var i=0; i<birdCount; i++) {
-    var bird = new Bird(s, this);
-    bird.position = new geo.Point(10, 10);
-    this.addChild(bird);
-    this.birds.push(bird);
-  }
 
   var origin;
   var cherryPopped = false;
@@ -69,12 +63,13 @@ function HappyLayer () {
     var newX = (coor.x - origin.x) * -2;
     var newY = (coor.y - origin.y) * 2;
 
-    console.log(newY);
     pos.x += newX;
     pos.y += newY;
 
-    if (data.coordinates.shoot) {
+    if (data.coordinates.f) {
+      console.log(pos);
       for(var i=0; i<birdCount; i++) {
+        console.log(self.birds[i].position);
         self.birds[i].shoot(pos);
       }
     }
